54ad41167ce84fc766a97af03c3b2ccb5e66961e
ChattiNader <chatti.tkd@gmail.com>
2019-10-01 23:42:39 +0200

    Optimisation de l'app Calculatrice (Méthodes et Style)

--- AndroidTP/Calculatrice/src/main/java/com/example/calculatrice/MainActivity.kt
+++ AndroidTP/Calculatrice/src/main/java/com/example/calculatrice/MainActivity.kt
@@ -1,207 +1,87 @@
 package com.example.calculatrice
 
 import androidx.appcompat.app.AppCompatActivity
-
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var showCalcul: TextView
-    private lateinit var button0: Button
-    private lateinit var button1: Button
-    private lateinit var button2: Button
-    private lateinit var button3: Button
-    private lateinit var button4: Button
-    private lateinit var button5: Button
-    private lateinit var button6: Button
-    private lateinit var button7: Button
-    private lateinit var button8: Button
-    private lateinit var button9: Button
-    private lateinit var buttonSupprimer: Button
-    private lateinit var buttonEgal: Button
-    private lateinit var buttonPlus: Button
-    private lateinit var buttonMoins: Button
-    private lateinit var buttonMultiplication: Button
-    private lateinit var buttonDivision: Button
-    private var champCalcul: String = ""
-    private var resultat1: Int = 0
-    private var resultat2: Int = 0
-    private lateinit  var codeResultat: String
-    private var total: Int = 0
-    private var signe: String = ""
-    private var codeEgal: String = "NON"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvExpression.text = "0"
 
-        // On récupère les éléments graphiques pour pouvoir les manipuler
-        this.showCalcul = findViewById(R.id.showCalcul)
-        this.button0 = findViewById(R.id.button0)
-        this.button1 = findViewById(R.id.button1)
-        this.button2 = findViewById(R.id.button2)
-        this.button3 = findViewById(R.id.button3)
-        this.button4 = findViewById(R.id.button4)
-        this.button5 = findViewById(R.id.button5)
-        this.button6 = findViewById(R.id.button6)
-        this.button7 = findViewById(R.id.button7)
-        this.button8 = findViewById(R.id.button8)
-        this.button9 = findViewById(R.id.button9)
-        this.buttonSupprimer = findViewById(R.id.buttonVirgul)
-        this.buttonEgal = findViewById(R.id.buttonEgal)
-        this.buttonPlus = findViewById(R.id.buttonPlus)
-        this.buttonMoins = findViewById(R.id.buttonMoins)
-        this.buttonMultiplication = findViewById(R.id.buttonMultiplication)
-        this.buttonDivision = findViewById(R.id.buttonDivision)
+        // Numéros
+        tvOne.setOnClickListener {appendOnExpression("1",true)}
+        tvTwo.setOnClickListener {appendOnExpression("2",true)}
+        tvThree.setOnClickListener {appendOnExpression("3",true)}
+        tvFour.setOnClickListener {appendOnExpression("4",true)}
+        tvFive.setOnClickListener {appendOnExpression("5",true)}
+        tvSix.setOnClickListener {appendOnExpression("6",true)}
+        tvSeven.setOnClickListener {appendOnExpression("7",true)}
+        tvEight.setOnClickListener {appendOnExpression("8",true)}
+        tvNine.setOnClickListener {appendOnExpression("9",true)}
+        tvZero.setOnClickListener {appendOnExpression("0",true)}
+        tvDot.setOnClickListener {appendOnExpression(".",true)}
 
-        //Puis on initilise la valeur du champ de texte showCalcul. Permettra d'afficher le calcul à l'écran
+        // Opérateurs
+        tvPlus.setOnClickListener {appendOnExpression("+",true)}
+        tvMinus.setOnClickListener {appendOnExpression("-",true)}
+        tvMul.setOnClickListener {appendOnExpression("*",true)}
+        tvDivide.setOnClickListener {appendOnExpression("/",true)}
+        tvOpen.setOnClickListener {appendOnExpression("(",true)}
+        tvClose.setOnClickListener {appendOnExpression(")",true)}
 
-        champCalcul = ""
-        codeResultat = "1"
-        signe = ""
-        showCalcul.text = champCalcul.toString()
-
-        //Mise en place des actionListner sur les boutons
-
-        button0.setOnClickListener { traitementCalcul(0) }
-
-        button1.setOnClickListener { traitementCalcul(1) }
-
-        button2.setOnClickListener { traitementCalcul(2) }
-
-        button3.setOnClickListener { traitementCalcul(3) }
-
-        button4.setOnClickListener { traitementCalcul(4) }
-
-        button5.setOnClickListener { traitementCalcul(5) }
-
-        button6.setOnClickListener { traitementCalcul(6) }
-
-        button7.setOnClickListener { traitementCalcul(7) }
-
-        button8.setOnClickListener { traitementCalcul(8) }
-
-        button9.setOnClickListener { traitementCalcul(9) }
-
-        buttonSupprimer.setOnClickListener {
-            // On supprime tout
-            signe = ""
-            resultat1 = 0
-            resultat2 = 0
-
-            resultat()
+        // Clear
+        tvClear.setOnClickListener {
+            tvExpression.text = "0"
+            tvResult.text = ""
         }
 
-        buttonPlus.setOnClickListener { traitementSigne("+") }
-
-        buttonMoins.setOnClickListener { traitementSigne("-") }
-
-        buttonMultiplication.setOnClickListener { traitementSigne("*") }
-
-        buttonDivision.setOnClickListener { traitementSigne("/") }
-
-        buttonEgal.setOnClickListener {
-            if (signe === "+") {
-                resultat1 = resultat1 + resultat2
-            } else if (signe === "-") {
-                resultat1 = resultat1 - resultat2
-            } else if (signe === "*") {
-                resultat1 = resultat1 * resultat2
-            } else if (signe === "/") {
-                resultat1 = resultat1 / resultat2
+        // Back
+        tvBack.setOnClickListener {
+            val string = tvExpression.text.toString()
+            if (string.isNotEmpty()) {
+                tvExpression.text = string.substring(0, string.length-1)
             }
-
-            codeEgal = "OUI"
-            signe = ""
-            resultat2 = 0
-
-            resultat()
+            tvResult.text = ""
         }
 
+        // Equals
+        tvEquals.setOnClickListener {
+            try {
+                val expression = ExpressionBuilder(tvExpression.text.toString()).build()
+                val result = expression.evaluate()
+                val longResult = result.toLong()
+                if (result == longResult.toDouble())
+                    tvResult.text = longResult.toString()
+                else
+                    tvResult.text = result.toString()
+            } catch (e: Exception) {
+                Log.d("Exception", " message: " + e.message)
+            }
+        }
     }
 
-    fun traitementCalcul(nombre: Int) {
-
-        // Si la variable chiffre est vide c'est que l'on est en train de rensigner la
-        // première composante de l'opération.
-        // Sinon c'est qu'on remplie la seconde
-
-        if (codeEgal === "OUI") {
-            codeEgal = "NON"
-            resultat1 = 0
-            resultat2 = 0
-            signe = ""
+    fun appendOnExpression(string: String, canClear: Boolean) {
+        if (tvExpression.text == "0") {
+            tvExpression.text = ""
         }
 
-        if (signe == "") {
+        if (tvResult.text.isNotEmpty()) {
+            tvExpression.text = tvResult.text
+        }
 
-            //Si le chiffre est inferrieur à dix c'est que c'est le premier que l'on rempli.
-            // sinon on multiplie le resultat par 10 avant d'additionner
-
-            if (resultat1 == 0) {
-                resultat1 = nombre
-            } else {
-                resultat1 = resultat1 * 10 + nombre
-            }
+        if(canClear) {
+            tvResult.text = ""
+            tvExpression.append(string)
         } else {
-            if (resultat2 == 0) {
-                resultat2 = nombre
-            } else {
-                resultat2 = resultat2 * 10 + nombre
-            }
+            tvExpression.append(tvResult.text)
+            tvExpression.append(string)
+            tvResult.text = ""
         }
-
-        resultat()
-
     }
-
-    fun traitementSigne(operateur: String) {
-
-        //Le code résultat permet de savoir si on remplie le prmier ou le deuxième champ.
-        //si il est à 1, on le passe à 2 et on initialise le second champ.
-        //Si il est à 2 on effectue l'opération et on continu
-
-        if (codeEgal === "OUI") { codeEgal = "NON" }
-
-        if (codeResultat === "1") {
-
-            codeResultat = "2"
-            signe = operateur
-
-        } else {
-
-            if (signe === "+") {
-                resultat1 = resultat1 + resultat2
-            } else if (signe === "-") {
-                resultat1 = resultat1 - resultat2
-            } else if (signe === "*") {
-                resultat1 = resultat1 * resultat2
-            } else if (signe === "/") {
-                resultat1 = resultat1 / resultat2
-            }
-
-            signe = operateur
-            resultat2 = 0
-        }
-
-        resultat()
-
-    }
-
-    fun resultat() {
-
-        // On affiche la saisie
-
-        if (resultat2 != 0) {
-            champCalcul = resultat1.toString() + signe + resultat2.toString()
-        } else {
-            champCalcul = resultat1.toString() + signe
-        }
-        showCalcul.text = champCalcul
-
-    }
-
 }
