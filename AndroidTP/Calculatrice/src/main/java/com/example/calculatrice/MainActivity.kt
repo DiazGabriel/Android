c38a3595ba566b73ed57535ac7b94507159b2fd6
HamzaJrad <hamza.jrad@gmail.com>
2019-10-03 12:12:15 +0200

    Correction main

--- AndroidTP/Calculatrice/src/main/java/com/example/calculatrice/MainActivity.kt
+++ AndroidTP/Calculatrice/src/main/java/com/example/calculatrice/MainActivity.kt
@@ -151,18 +151,4 @@ class MainActivity : AppCompatActivity() {
         }
         isUp = !isUp
     }
-
-    fun history(args: Array<String>){
-
-        val arrayList = ArrayList<String>()//Creating an empty arraylist
-        arrayList.add("Ajay")//Adding object in arraylist
-        arrayList.add("Vijay")
-        arrayList.add("Prakash")
-        arrayList.add("Rohan")
-        arrayList.add("Vijay")
-        println(".......print ArrayList.......")
-        for (i in arrayList) {
-            println(i)
-        }
-    }
 }
